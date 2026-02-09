d3d210ef906255dc61d4d06068f117d59d43a576
t3chlust <t3chlust@gmail.com>
2026-02-10 02:07:54 +0500

    Initial commit

---
+++ BeautySalon/AppointmentPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BeautySalon
+{
+    /// <summary>
+    /// Логика взаимодействия для AppointmentPage.xaml
+    /// </summary>
+    public partial class AppointmentPage : Page
+    {
+        private int maxPage;
+        private int page;
+        private int perPage = 9;
+        private List<Appointment> appointments;
+
+        public AppointmentPage()
+        {
+            InitializeComponent();
+            appointments = beauty_salonEntities.GetInstance().Appointment.OrderBy(x => x.id).ToList();
+            page = maxPage = (int)Math.Ceiling((double)appointments.Count() / perPage);
+            MoveToPage(page);
+        }
+
+        private void MoveToPage(int page)
+        {
+            PageStatusTextBlock.Text = $"{page} / {maxPage}";
+            AppointmentListView.ItemsSource = appointments.Skip((page - 1) * perPage).Take(perPage).ToList();
+            if (page <= 1)
+            {
+                PreviousPageButton.IsEnabled = false;
+            } else
+            {
+                PreviousPageButton.IsEnabled = true;
+            }
+            if (page >= maxPage)
+            {
+                NextPageButton.IsEnabled = false;
+            } else
+            {
+                NextPageButton.IsEnabled = true;
+            }
+            //if (page < 1)
+            //{
+            //    PreviousPageButton.IsEnabled = true;
+            //} else if (page == 1)
+            //{
+            //    PreviousPageButton.IsEnabled = false;
+            //}
+            //if (page >= maxPage)
+            //{
+            //    NextPageButton.IsEnabled = true;
+            //} else
+            //{
+            //    NextPageButton.IsEnabled = false;
+            //}
+        }
+
+        private void PreviousPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            page = page - 1;
+            MoveToPage(page);
+        }
+
+        private void NextPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            page = page + 1;
+            MoveToPage(page);
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new CreateAppointmentPage());
+        }
+    }
+}
---
+++ BeautySalon/CreateAppointmentPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BeautySalon
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateAppointmentPage.xaml
+    /// </summary>
+    public partial class CreateAppointmentPage : Page
+    {
+        public CreateAppointmentPage()
+        {
+            InitializeComponent();
+            ClientListBox.ItemsSource = beauty_salonEntities.GetInstance().Client.ToList();
+            EmployeeListBox.ItemsSource = beauty_salonEntities.GetInstance().Employee.ToList();
+            ServiceListBox.ItemsSource = beauty_salonEntities.GetInstance().Service.ToList();
+            StatusListBox.ItemsSource = beauty_salonEntities.GetInstance().AppointmentStatus.ToList();
+            MainDatePicker.SelectedDate = DateTime.Now.Date;
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ClientListBox.SelectedItem == null || EmployeeListBox.SelectedItem == null || ServiceListBox.SelectedItem == null ||
+                StatusListBox.SelectedItem == null || MainDatePicker == null)
+            {
+                MessageBox.Show("Заполните все поля", "Валидация данных", MessageBoxButton.OK, MessageBoxImage.Information);
+            } else
+            {
+                var appointment = new Appointment()
+                {
+                    client = ((Client)ClientListBox.SelectedItem).id,
+                    employee = ((Employee)EmployeeListBox.SelectedItem).id,
+                    service = ((Service)ServiceListBox.SelectedItem).id,
+                    status = ((AppointmentStatus)StatusListBox.SelectedItem).id,
+                    date = MainDatePicker.SelectedDate ?? DateTime.Now.Date
+                };
+                beauty_salonEntities.GetInstance().Appointment.Add(appointment);
+                beauty_salonEntities.GetInstance().SaveChanges();
+                MessageBox.Show("Запись успешно создана.", "Создание записи", MessageBoxButton.OK, MessageBoxImage.Information);
+                ClientListBox.SelectedItem = null;
+                EmployeeListBox.SelectedItem = null;
+                ServiceListBox.SelectedItem = null;
+                StatusListBox.SelectedItem = null;
+                MainDatePicker.SelectedDate = DateTime.Now.Date;
+            }
+        }
+    }
+}
